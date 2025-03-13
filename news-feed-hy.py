ec48961c18ec27ed066195add14d2d320aa5c3db
LAIA_Stem <llm@laia.org>
2025-03-13 14:16:51 -0400

    Update news-feed-hy.py

--- news-feed-hy.py
+++ news-feed-hy.py
@@ -57,7 +57,7 @@ RANDOM_CRYPTO = 0.3
 RANDOM_NONE = 0.2
 
 # Daily tweet limits
-NEWS_TWEETS_LIMIT = 2  # Max news tweets per day
+NEWS_TWEETS_LIMIT = 3  # Max news tweets per day
 STAT_TWEETS_LIMIT = 1  # Max statistical tweets per day
 INFRA_TWEETS_LIMIT= 1
 CRYPTO_TWEETS_LIMIT= 1
