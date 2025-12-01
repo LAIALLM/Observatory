ad23bc6c7c66dfb532fd28d403261b215da8e96d
LAIA_Stem <llm@laia.org>
2025-12-01 14:01:13 +0100

    Update news-feed-hy.py

--- news-feed-hy.py
+++ news-feed-hy.py
@@ -90,12 +90,12 @@ RETENTION_DAYS = 10  # Remove news older than 10 days
 TWEET_THRESHOLD = 9 # Define score threshold for tweets
 
 # Random tweets probabilities
-RANDOM_NEWS = 0
-RANDOM_STATISTIC = 0
-RANDOM_INFRASTRUCTURE = 0
-RANDOM_CRYPTO = 0
-RANDOM_REPLY = 1
-RANDOM_NONE = 0
+RANDOM_NEWS = 0.2
+RANDOM_STATISTIC = 0.1
+RANDOM_INFRASTRUCTURE = 0.1
+RANDOM_CRYPTO = 0.2
+RANDOM_REPLY = 0.2 
+RANDOM_NONE = 0.2
 
 # Daily tweet limits
 NEWS_TWEETS_LIMIT = 3  # Max news tweets per day
